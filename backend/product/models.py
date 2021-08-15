4bfcaf8eaada71a5c81ffa1540dd8e5fb2dab4ea
Lucas Ferreira <lucasferreiraek@gmail.com>
2021-08-15 01:41:22 -0300

    Add product app

--- backend/product/models.py
+++ backend/product/models.py
@@ -4,12 +4,19 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    image = models.ImageField(upload_to='shirt_images')
+
+
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=50, verbose_name=_('Título'))
     slug = models.SlugField(max_length=50, verbose_name=_('Slug'))
     description = models.TextField(verbose_name=_('Descrição'))
     price = models.DecimalField(max_digits=5, decimal_places=2, verbose_name=_('Preço'))
+    thumbnail = models.ImageField(upload_to='thumbnails')
+    images = models.ManyToManyField(Image)
 
     class Meta:
         verbose_name = _('Produto')
